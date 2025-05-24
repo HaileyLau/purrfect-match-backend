3739c8c9e30aa5ff944f81561834c8f054b506ce
Vanessa Li <vanel15@uci.edu>
2025-05-24 15:33:51 -0700

    database first three

---
+++ data/sql.sql
@@ -0,0 +1,18 @@
+CREATE TABLE CATS(
+    name TEXT,
+    age TEXT,
+    breed TEXT,
+    gender TEXT,
+    photo TEXT,
+    bio TEXT NULL
+);
+
+INSERT INTO CATS (name, age, breed, gender, photo)
+VALUES ('Andy', 'Adult', 'Domestic Long Hair', 'Male', 'https://cdn.rescuegroups.org/2221/pictures/animals/21424/21424800/100784220.jpg');
+
+INSERT INTO CATS (name, age, breed, gender, photo)
+VALUES ('Nala', 'Adult', 'Domestic Short Hair', 'Female', 'https://cdn.rescuegroups.org/2221/pictures/animals/21488/21488646/100920676.jpg');
+
+
+INSERT INTO CATS (name, age, breed, gender, photo)
+VALUES ('Kate', 'Young', 'Domestic Short Hair', 'Female', 'https://cdn.rescuegroups.org/2221/pictures/animals/21489/21489058/100921263.jpg');
