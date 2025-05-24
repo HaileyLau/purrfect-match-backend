5987c169194ac3a763258fe596cb1df08d8c8cfb
Hailey <72575893+HaileyLau@users.noreply.github.com>
2025-05-24 16:25:54 -0700

    Changed user to cat

--- app.py
+++ app.py
@@ -8,32 +8,38 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Cat(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique = True, nullable=False)
+    breed = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.String(80), nullable=False)
+    gender = db.Column(db.String(80), nullable=False)
+    photo = db.Column(db.String(120), nullable=False)
 
 with app.app_context():
     db.create_all()
 
 @app.route('/') # READ
 def index():
-    users = User.query.all()
+    users = Cat.query.all()
     return render_template('index.html', users=users)
 
 @app.route('/add', methods=['POST']) # CREATE
 def add_user():
     name = request.form['name']
-    email = request.form['email']
-    user = User(name=name, email=email)
-    db.session.add(user)
+    breed = request.form['breed']
+    age = request.form['age']
+    gender = request.form['gender']
+    photo = request.form['photo']
+    cat = Cat(name=name, breed=breed, age=age, gender=gender, photo=photo)
+    db.session.add(cat)
     db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/update', methods=['POST']) # UPDATE
 def update_user():
     user_id = request.form['id']
-    user = User.query.get(user_id)
+    user = Cat.query.get(user_id)
     if user:
         user.name = request.form['name']
         user.email = request.form['email']
@@ -42,7 +48,7 @@ def update_user():
 
 @app.route('/delete/<int:id>') # DELETE
 def delete_user(id):
-    user = User.query.get(id)
+    user = Cat.query.get(id)
     if user:
         db.session.delete(user)
         db.session.commit()
